655734f46f18c867730c3e3b40d95ad6c56b151b
nandang <net.nandang@gmail.com>
2025-03-10 23:28:04 +0700

    * add test for deeply nested folders * add test for handling Errno::ENOENT

--- spec/duplicate_file_finder_spec.rb
+++ spec/duplicate_file_finder_spec.rb
@@ -1,31 +1,38 @@
-require_relative '../duplicate_file_finder' # Import the main script
+require_relative '../duplicate_file_finder'
 require 'fileutils'
 
 RSpec.describe DuplicateFileFinder do
-  let(:test_dir) { "spec/test_files" } # Define a test directory
+  let(:test_dir) { "spec/test_files" }
 
   before do
-    FileUtils.mkdir_p(test_dir) # Create the test folder
+    # Nested folders, lets try :p
+    FileUtils.mkdir_p("#{test_dir}/folder1/folder1.1")
+    FileUtils.mkdir_p("#{test_dir}/folder2")
 
-    # Create files with duplicate content
+    # Create files in root directory
     File.write("#{test_dir}/file1.txt", "abcdef")
     File.write("#{test_dir}/file2.txt", "abcdef")
-    File.write("#{test_dir}/file3.txt", "abcdef")
 
-    # Create files with different content
-    File.write("#{test_dir}/file4.txt", "xyz123")
-    File.write("#{test_dir}/file5.txt", "xyz123")
-    File.write("#{test_dir}/file6.txt", "hello")
+    # Create files in folder1
+    File.write("#{test_dir}/folder1/file1.txt", "abcdef")
+    File.write("#{test_dir}/folder1/file2.txt", "xyz123")
+
+    # Create files in folder1.1
+    File.write("#{test_dir}/folder1/folder1.1/file1.txt", "abcdef")
+    File.write("#{test_dir}/folder1/folder1.1/file2.txt", "xyz123")
+
+    # Create files in folder2
+    File.write("#{test_dir}/folder2/file1.txt", "hello")
   end
 
   after do
-    FileUtils.rm_rf(test_dir) # Clean up the test folder after each test
+    FileUtils.rm_rf(test_dir)
   end
 
-  it "counts files with the same content" do
+  it "counts files with the same content across nested directories" do
     finder = DuplicateFileFinder.new(test_dir)
 
-    expect { finder.run }.to output("abcdef 3\n").to_stdout
+    expect { finder.run }.to output("abcdef 4\n").to_stdout
   end
 
   it "returns a message if no duplicate files are found" do
@@ -37,5 +44,9 @@ RSpec.describe DuplicateFileFinder do
     expect { finder.run }.to output("No duplicate files found\n").to_stdout
   end
 
+
+  it "returns an error when the directory does not exist" do
+    expect { DuplicateFileFinder.new("invalid_dir") }.to raise_error(ArgumentError, "Error: Directory 'invalid_dir' does not exist.")
+  end
 end
 
